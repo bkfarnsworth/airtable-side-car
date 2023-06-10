f408a08477afdc1cf76aef4ad7c6173d950617a5
Brian Farnsworth <farnswor@adobe.com>
2023-06-10 00:25:06 -0600

    got basic side car view working

---
+++ frontend/index.js
@@ -0,0 +1,46 @@
+import {
+    initializeBlock,
+    useBase,
+    CellRenderer,
+    useCursor, 
+    useLoadable, 
+    useWatchable,
+    useRecordById,
+} from '@airtable/blocks/ui';
+import React from 'react';
+
+
+function SideCarApp() {
+    const base = useBase();
+    const cursor = useCursor();
+    const table = base.getTableById(cursor.activeTableId);
+    
+    useLoadable(cursor); // load selected records and fields
+    useWatchable(cursor, ['selectedRecordIds']); // re-render whenever the list of selected records changes
+
+    let recordId = cursor.selectedRecordIds[0] || '';
+    let record = useRecordById(table, recordId);
+
+    if(!record){
+        return <div>No record selected</div>;
+    }    
+
+    // Selected records: {cursor.selectedRecordIds.join(', ')}
+    // Selected fields: {cursor.selectedFieldIds.join(', ')}
+
+    return (
+        <div style={{overflowWrap: 'anywhere'}}>
+            <b>Name:</b>
+            <div>{record.name}</div>
+            <br/>
+            <b>Link:</b>
+            <div><a target="_blank" href={record.getCellValue("Link")}>{record.getCellValue("Link")}</a></div>
+            <br/>
+            <b>Notes:</b>
+            <CellRenderer record={record} field={table.getFieldByName("Notes")}/>
+            <br/>
+        </div>
+    );
+}
+
+initializeBlock(() => <SideCarApp />);
