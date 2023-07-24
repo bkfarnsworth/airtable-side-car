ab05a8a8bb16f7e741c02047ecfe8726fcf98e7f
Brian Farnsworth <farnswor@adobe.com>
2023-07-23 20:02:24 -0600

    Scroll the side car extension into view whenever selected records changes

--- frontend/index.js
+++ frontend/index.js
@@ -16,8 +16,13 @@ function SideCarApp() {
     const table = base.getTableById(cursor.activeTableId);
     const tagsField = table.getFieldByName("Tags");
     
-    useLoadable(cursor); // load selected records and fields
-    useWatchable(cursor, ['selectedRecordIds']); // re-render whenever the list of selected records changes
+    // load selected records and fields
+    useLoadable(cursor); 
+
+    // re-render whenever the list of selected records changes
+    useWatchable(cursor, ['selectedRecordIds'],() => {
+        document.querySelector('#side-car-extension-container')?.scrollIntoView();
+   }); 
 
     let recordId = cursor.selectedRecordIds[0] || '';
     let record = useRecordById(table, recordId);
@@ -30,7 +35,7 @@ function SideCarApp() {
     // Selected fields: {cursor.selectedFieldIds.join(', ')}
 
     return (
-        <div style={{overflowWrap: 'anywhere'}}>
+        <div id="side-car-extension-container" style={{overflowWrap: 'anywhere'}}>
             <b>Name:</b>
             <div>{record.name}</div>
             <br/>
